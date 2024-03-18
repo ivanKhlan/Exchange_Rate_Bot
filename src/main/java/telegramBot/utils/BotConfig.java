4fdd6a2100824f8e103181e6b668b6880304d8a0
baby4dm <babych.dmtr@gmail.com>
2024-03-18 16:17:51 +0200

    added notifications

--- src/main/java/telegramBot/utils/BotConfig.java
+++ src/main/java/telegramBot/utils/BotConfig.java
@@ -2,7 +2,7 @@ package telegramBot.utils;
 
 public class BotConfig {
 
-    public static final String BOT_NAME  = "tproject_test_bot";
+    public static final String BOT_NAME  = "";
 
-    public static final String BOT_TOKEN = "7132888808:AAGFfp4xyPY6MkvmivrX4D0OxXxWpZkZzc8";
+    public static final String BOT_TOKEN = "";
 }
